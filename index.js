0fc531bbf497355c7ca33fb77135e737fe3bad88
Christopher Gillis <chrisg@socialtables.com>
2014-08-27 23:26:41 -0400

    Added simple functions and tests

---
+++ index.js
@@ -0,0 +1,69 @@
+
+/**
+ * Calculates the mean of an array of numbers
+ */
+exports.mean = mean;
+
+function mean(vals){
+	if(!isValid(vals)){
+		return null;
+	}
+
+	var valSum = sum(vals);
+
+	return valSum / vals.length;
+}
+
+function sum(vals){
+	return vals.reduce(next, 0);
+
+	function next(acc, value){
+		return acc + value;
+	}
+}
+
+exports.variance = variance;
+
+function variance(vals){
+	if(!isValid(vals)){
+		return null;
+	}
+
+	var valMean = mean(vals);
+
+	var squaredDifferences = vals.map(function(value){
+		return subtractAndSquare(value);
+	});
+
+	var sumOfSquaredDifferences = sum(squaredDifferences);
+
+	return sumOfSquaredDifferences / vals.length;
+
+	function subtractAndSquare(x){
+		return (x - valMean) * (x - valMean);
+	}
+}
+
+exports.standardDeviation = standardDeviation;
+
+function standardDeviation(vals){
+	var valVariance = variance(vals);
+
+	return valVariance && Math.sqrt(valVariance);
+}
+
+function isValid(vals){
+	if(!vals || !vals.length){
+		return null;
+	}
+
+	if(!vals.every(isNum)){
+		return false;
+	}
+
+	return true;
+
+	function isNum(value){
+		return !isNaN(value);
+	}
+}
---
+++ test/tests.js
@@ -0,0 +1,109 @@
+var expect = require('totes').expect;
+
+var stats = require('../index');
+
+describe('mean', function(){
+	it('should return null for an empty list', function(){
+		var mean = stats.mean([]);
+
+		expect(mean).isExactly(null);
+	});
+
+	it('should return null when any item is not a number', function(){
+		var mean = stats.mean([1,2,3,'a']);
+
+		expect(mean).isExactly(null);
+
+		var mean2 = stats.mean([1,2,null,function(){}]);
+
+		expect(mean2).isExactly(null);
+	});
+
+	it('should return 2 when the input is [2]', function(){
+		var mean = stats.mean([2]);
+
+		expect(mean).is(2);
+	});
+
+	it('should return 2 when the input is [1,3]', function(){
+		var mean = stats.mean([1,3]);
+
+		expect(mean).is(2);
+	});
+});
+
+var dataset = [9, 2, 5, 4, 12, 7, 8, 11, 9, 3, 7, 4, 12, 5, 4, 10, 9, 6, 9, 4];
+
+describe('variance', function(){
+	it('should return null for an empty list', function(){
+		var variance = stats.variance([]);
+
+		expect(variance).isExactly(null);
+	});
+
+	it('should return null when any item is not a number', function(){
+		var variance = stats.variance([1,2,3,'a']);
+
+		expect(variance).isExactly(null);
+
+		var variance2 = stats.variance([1,2,null,function(){}]);
+
+		expect(variance2).isExactly(null);
+	});
+
+	it('should return 0 for a list with all the same numbers', function(){
+		var variance = stats.variance([1,1,1,1,1,1]);
+
+		expect(variance).isZero();
+
+		var variance2 = stats.variance([5,5,5,5,5,5]);
+
+		expect(variance2).isZero();
+	});
+
+	it('should return the correct value', function(){
+		// example solution from http://chemistry.about.com/od/workedchemistryproblems/a/Population-Standard-Deviation-Example-Calculation.htm
+
+		var variance = stats.variance(dataset);
+
+		expect(variance).is(8.9);
+	});
+});
+
+describe('standardDeviation', function(){
+	it('should return null for an empty list', function(){
+		var standardDeviation = stats.standardDeviation([]);
+
+		expect(standardDeviation).isExactly(null);
+	});
+
+	it('should return null when any item is not a number', function(){
+		var standardDeviation = stats.standardDeviation([1,2,3,'a']);
+
+		expect(standardDeviation).isExactly(null);
+
+		var standardDeviation2 = stats.standardDeviation([1,2,null,function(){}]);
+
+		expect(standardDeviation2).isExactly(null);
+	});
+
+	it('should return 0 for a list with all the same numbers', function(){
+		var standardDeviation = stats.standardDeviation([1,1,1,1,1,1]);
+
+		expect(standardDeviation).isZero();
+
+		var standardDeviation2 = stats.standardDeviation([5,5,5,5,5,5]);
+
+		expect(standardDeviation2).isZero();
+	});
+
+	it('should return the correct value', function(){
+		// example solution from http://chemistry.about.com/od/workedchemistryproblems/a/Population-Standard-Deviation-Example-Calculation.htm
+
+		var standardDeviation = stats.standardDeviation(dataset);
+
+		expect(standardDeviation).is(Math.sqrt(8.9));
+	});
+
+});
+
